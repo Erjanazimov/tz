46fa0f55d94aedbf589cd916b209f1cf59405e30
erjan azimov <erjanazimov@MacBook-Air-erjan.local>
2022-03-27 23:18:31 +0600

    e

--- src/pages/Public.js
+++ src/pages/Public.js
@@ -25,30 +25,35 @@ const Public = () => {
         dispatch(fetchPublicNext({name: userState.user_metadata.user_name, count: num}))
     }
 
-    return (
-        <div className="wid">
-            <h2>Публичные репозитории</h2>
-            <Collapse accordion>
+    if (userState) {
+        return (
+            <div className="wid">
+                <h2>Публичные репозитории</h2>
+                <Collapse accordion>
 
-                {publicState.public.length ?
-                    publicState.public.map(item => {
-                        return  <Panel header={item.full_name} key={item.id}>
-                            <p>Название: <b>{item.full_name} </b></p>
-                            <p>Ссылка: <a href={item.html_url}>{item.html_url}</a> </p>
-                        </Panel>
-                    })
-                    : <h2>Загрузка...</h2>
+                    {publicState.public.length ?
+                        publicState.public.map(item => {
+                            return <Panel header={item.full_name} key={item.id}>
+                                <p>Название: <b>{item.full_name} </b></p>
+                                <p>Ссылка: <a href={item.html_url}>{item.html_url}</a></p>
+                            </Panel>
+                        })
+                        : <h2>Загрузка...</h2>
+                    }
+
+                </Collapse>
+                {publicState.nextPublic ?
+                    <div className="mt-3">
+                        <button onClick={nextPublic} type="button"
+                                className="btn btn-outline-secondary form-control">Загрузить еще
+                        </button>
+                    </div> : null
                 }
 
-            </Collapse>
-            {publicState.nextPublic ?
-                <div className="mt-3">
-                    <button onClick={nextPublic} type="button" className="btn btn-outline-secondary form-control">Загрузить еще</button>
-                </div> : null
-            }
+            </div>
+        );
 
-        </div>
-    );
+    }
 };
 
 export default Public;
